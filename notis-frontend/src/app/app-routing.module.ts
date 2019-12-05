ab126c5adea28af7ebbb65d96693924cfadc169d
Chilaboc Ecaterina Mihaela <dev.c.ecaterina.mihaela@gmail.com>
2019-12-05 16:46:55 +0200

    come back to the empty project

--- notis-frontend/src/app/app-routing.module.ts
+++ notis-frontend/src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {PokemonListComponent} from './pokemon-list/pokemon-list.component';
-import {PokemonDetailsComponent} from './pokemon-details/pokemon-details.component';
 
 const appRoutes: Routes = [
-  {
-    path: 'pokemons',
-    component: PokemonListComponent
-  },
   {
     path: '',
     redirectTo: '/',
--- notis-frontend/src/app/app.module.ts
+++ notis-frontend/src/app/app.module.ts
@@ -4,14 +4,10 @@ import {HttpClientModule} from '@angular/common/http';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {PokemonListComponent} from './pokemon-list/pokemon-list.component';
-import {PokemonDetailsComponent} from './pokemon-details/pokemon-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PokemonListComponent,
-    PokemonDetailsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
